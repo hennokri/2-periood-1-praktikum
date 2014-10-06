5f18493be0551d889c671e8b6240cec3e3005984
Kristi Henno <hennokri@gmail.com>
2014-10-06 11:03:56 +0300

    link viib mujale
    link viib mujale

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML>
+  <head>
+    <title>1 praktikum | esileht</title>
+    <meta charset="utf-8">
+	
+  </head>
+  
+  <body>
+   <h2>See on algus</h2>
+   <a href="2ndpage.php">a see on link mis peaks meid edasi viima</a>
+   </body>
+</html>
