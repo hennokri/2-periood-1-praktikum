6737acc980f2e4875f76496cf181b9dd200bdf3e
Kristi Henno <hennokri@gmail.com>
2014-10-06 11:04:39 +0300

    link toob tagasi
    link toob tagasi

---
+++ 2ndpage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+  <head>
+    <title>1 praktikum | teine leht</title>
+    <meta charset="utf-8">
+	
+  </head>
+  
+  <body>
+   <h3>...see on koht kuhu link meid viis. </h3>
+<a href="index.php">tagasi saab siit</a>
+   </body>
+</html>
