52645cfc17e246880764253e5473bd5fe09f1a21
Kristi Henno <hennokri@gmail.com>
2014-10-16 10:54:23 +0300

    ...
    ...

--- 2ndpage.php
+++ 2ndpage.php
@@ -1,12 +1,19 @@
 <!DOCTYPE HTML>
   <head>
     <title>1 praktikum | teine leht</title>
-    <meta charset="utf-8">
+ <meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
 	
   </head>
   
   <body>
+ 	
    <h3>...see on koht kuhu link meid viis. </h3>
-<a href="index.php">tagasi saab siit</a>
+<a href="index.php?name=kristi&age=25">tagasi saab siit</a>
+<?php
+//$_GET
+
+print_r($_GET);
+?>
    </body>
 </html>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE HTML>
   <head>
     <title>1 praktikum | esileht</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8">
 	
   </head>
@@ -9,5 +10,7 @@
   <body>
    <h2>See on algus</h2>
    <a href="2ndpage.php">a see on link mis peaks meid edasi viima</a>
+  <?php echo ($_GET);
+  ?>
    </body>
 </html>
